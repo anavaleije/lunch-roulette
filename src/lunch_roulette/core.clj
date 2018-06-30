91c8f2706d5e10dea37bf2afed56268a270efd55
Ana Valeije <ana.valeije@nubank.com.br>
2018-06-30 15:55:19 -0300

    Generate slack message for sampled groups

--- src/lunch_roulette/core.clj
+++ src/lunch_roulette/core.clj
@@ -16,13 +16,24 @@
   (let [{:keys [restaurant]} (apply max-key (partial scoring/score person) (vals groups))]
     (update-in groups [restaurant :people] #(conj % person-key))))
 
+(defn format-slack-message [results all-people all-restaurants]
+  (->> results
+       (mapv (fn [{:keys [people restaurant]}]
+               (->> people
+                    (mapv #(get-in all-people [% :slack]))
+                    (clojure.string/join " ")
+                    (str (get-in all-restaurants [restaurant :name]) " - "))))
+       (clojure.string/join "\n")))
+
 (defn sample-next-event-groups []
-  (let [groups (sample-restaurant-groups data/n-groups (keys data/restaurants))]
-    (-> (reduce
-          allocate-person
-          groups
-          data/people)
-        vals)))
+  (let [groups (sample-restaurant-groups data/n-groups (keys data/restaurants))
+        results (-> (reduce
+                      allocate-person
+                      groups
+                      data/people)
+                    vals)]
+    (println results)
+    (println (format-slack-message results data/people data/restaurants))))
 
 (comment
   (sample-next-event-groups))
